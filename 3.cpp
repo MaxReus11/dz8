e6579fc56fddae5c0ebaeeaf43b64bf9c43720ad
MaxReus11 <64498905+MaxReus11@users.noreply.github.com>
2021-04-01 16:30:16 +0300

    threadsafe priority_queue

--- 3.cpp
+++ 3.cpp
@@ -2,111 +2,111 @@
 #include <memory>
 #include <mutex>
 #include <queue>
-
-template < typename T >
-class Threadsafe_Queue
+template < typename T, typename Comp >
+class Threadsafe_Priority_Queue
 {
 public:
 
-	Threadsafe_Queue() = default;
+    Threadsafe_Priority_Queue() = default;
 
-	Threadsafe_Queue(const Threadsafe_Queue & other)
-	{
-		std::lock_guard < std::mutex > lock(other.m_mutex);
-		m_queue = other.m_queue;
-	}
+    Threadsafe_Priority_Queue(const Threadsafe_Priority_Queue & other)
+    {
+        std::lock_guard < std::mutex > lock(other.m_mutex);
+        m_queue = other.m_queue;
+    }
 
-	Threadsafe_Queue & operator=(const Threadsafe_Queue & other)
-	{
-		std::scoped_lock lock(m_mutex, other.m_mutex);
-		m_queue = other.m_queue;
-	}
+    Threadsafe_Priority_Queue & operator=(const Threadsafe_Priority_Queue & other)
+    {
+        std::scoped_lock lock(m_mutex, other.m_mutex);
+        m_queue = other.m_queue;
+    }
 
 public:
 
-	void push(T value)
-	{
-		std::lock_guard < std::mutex > lock(m_mutex);
-		m_queue.push(value);
-		m_condition_variable.notify_one();
-	}
+    void push(T value)
+    {
+        std::lock_guard < std::mutex > lock(m_mutex);
+        m_queue.push(value);
 
-	void wait_and_pop(T & value)
-	{
-		std::unique_lock < std::mutex > lock(m_mutex);
-		
-		m_condition_variable.wait(lock, [this] {return !m_queue.empty(); });
-		value = m_queue.front();
-		m_queue.pop();
-	}
+        m_condition_variable.notify_one();
+    }
 
-	std::shared_ptr < T > wait_and_pop()
-	{
-		std::unique_lock < std::mutex > lock(m_mutex);
-		
-		m_condition_variable.wait(lock, [this] {return !m_queue.empty(); });
-		auto result = std::make_shared < T > (m_queue.front());
-		m_queue.pop();
-		
-		return result;
-	}
+    void wait_and_pop(T & value)
+    {
+        std::unique_lock < std::mutex > lock(m_mutex);
 
-	bool try_pop(T & value)
-	{
-		std::lock_guard < std::mutex > lock(m_mutex);
-		
-		if (m_queue.empty())
-		{
-			return false;
-		}
+        m_condition_variable.wait(lock, [this] {return !m_queue.empty(); });
+        value = m_queue.top();
+        m_queue.pop();
+    }
 
-		value = m_queue.front();
-		m_queue.pop();
+    std::shared_ptr < T > wait_and_pop()
+    {
+        std::unique_lock < std::mutex > lock(m_mutex);
 
-		return true;
-	}
+        m_condition_variable.wait(lock, [this] {return !m_queue.empty(); });
+        auto result = std::make_shared < T > (m_queue.top());
+        m_queue.pop();
 
-	std::shared_ptr < T > try_pop()
-	{
-		std::lock_guard < std::mutex > lock(m_mutex);
-		
-		if (m_queue.empty())
-		{
-			return std::shared_ptr < T > ();
-		}
-			
-		auto result = std::make_shared < T > (m_queue.front());
-		m_queue.pop();
-		
-		return result;
-	}
+        return result;
+    }
 
-	bool empty() const
-	{
-		std::lock_guard < std::mutex > lock(m_mutex);
-		return m_queue.empty();
-	}
+    bool try_pop(T & value)
+    {
+        std::lock_guard < std::mutex > lock(m_mutex);
+
+        if (m_queue.empty())
+        {
+            return false;
+        }
+
+        value = m_queue.top();
+        m_queue.pop();
+
+        return true;
+    }
+
+    std::shared_ptr < T > try_pop()
+    {
+        std::lock_guard < std::mutex > lock(m_mutex);
+
+        if (m_queue.empty())
+        {
+            return std::shared_ptr < T > ();
+        }
+
+        auto result = std::make_shared < T > (m_queue.top());
+        m_queue.pop();
+
+        return result;
+    }
+
+    bool empty() const
+    {
+        std::lock_guard < std::mutex > lock(m_mutex);
+        return m_queue.empty();
+    }
 
 private:
 
-	std::queue < T >		m_queue;
-	std::condition_variable m_condition_variable;
-	mutable std::mutex m_mutex;
+    std::priority_queue < T , std::vector<T>, Comp>	m_queue;
+    std::condition_variable m_condition_variable;
+    mutable std::mutex m_mutex;
 };
 
 int main(int argc, char ** argv)
 {
-	Threadsafe_Queue < int > queue;
+    Threadsafe_Priority_Queue < int, std::greater<int>> queue;
 
-	queue.push(42);
+    queue.push(42);
 
-	auto ptr = queue.wait_and_pop();
+    auto ptr = queue.wait_and_pop();
 
-	int value;
+    int value;
 
-	bool result = queue.try_pop(value);
+    bool result = queue.try_pop(value);
 
-	system("pause");
+    system("pause");
 
-	return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
